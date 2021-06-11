531a15096c6da496e31f5bc2490228047b083b42
Oleg-Kolesnik <71118695+Oleg-Kolesnik@users.noreply.github.com>
2021-06-11 20:53:33 +0300

    create dot delete

--- script.js
+++ script.js
@@ -1,75 +1,109 @@
-window.onload = (function () {
-  const calculator = document.getElementsByClassName('container')[0];
-  const input = document.getElementsByClassName('input')[0];
+window.onload = function () {
+  const calculator = document.getElementsByClassName("container")[0];
+  const input = document.getElementsByClassName("input")[0];
 
   let prevValue = "";
+  let currentValue = "";
   let currentOperator = "";
 
   function clear() {
     input.innerText = "";
   }
-  function plus() {
-    // input.innerText = 
-  }
-  function calculate () {
+
+  function calculate() {
     let result = "";
-    if(currentOperator === "+"){
-     result = +prevValue + +input.innerText;
+    if (currentOperator === "+") {
+      result = +prevValue + +currentValue;
+    }
+    if (currentOperator === "-") {
+      result = +prevValue - +currentValue;
+    }
+    if (currentOperator === "*") {
+      result = +prevValue * +currentValue;
+    }
+    if (currentOperator === "/") {
+      result = +prevValue / +currentValue;
     }
     input.innerText = result;
-    currentOperator = "";
+    // currentOperator = "";
+    prevValue = result;
   }
+
   const setPrevValue = () => {
-    prevValue = input.innerText;
+    if (!currentOperator) {
+      prevValue = input.innerText;
+    } else {
+      calculate();
+    }
   };
+
   const setCurrentOperator = (text) => {
     currentOperator = text;
+    input.innerText = text;
   };
 
-  calculator.addEventListener('click', (e) => {
-    if (e.target.className === "numbers") {
-      // console.log(e.target.innerText);
-      let text = input.innerText;
-      let targetText = e.target.innerText;
-      let newText = text + targetText;
-      input.innerText = newText;
+  const setCurrentValue = (value) => {
+    currentValue = value;
+  };
+
+  const isOp = (op) => {
+    if (op === "+" || op === "-" || op === "*" || op === "/") {
+      return true;
     }
-    if (e.target.className === 'operators') {
-      let text = e.target.innerText;
+    return false;
+  };
 
-
-      switch (text) {
-        case '+':
-          setPrevValue();
-          clear();
-          setCurrentOperator("+");
-          console.log(prevValue, currentOperator);
-          break;
-        case '-':
-          console.log(text);
-          break;
-        case '*':
-          console.log(text);
-          break;
-        case '/':
-          console.log(text);
-          break;
-        // case 'C':
-        //   console.log(text);
-        //   break;
-        case '=':
-          calculate();
-          console.log(text);
-          break;
-
-        default:
-          clear();
-          break;
+  calculator.addEventListener("click", (e) => {
+    // console.log(e.target.innerText);
+    if (e.target.className === "numbers") {
+      let text = input.innerText;
+      if (isOp(text)) {
+        clear();
+        text = "";
       }
 
-    }
-    // console.log(input.innerText);
-  });
+      let targetText = e.target.innerText;
+      if(targetText === 0){
+          //  if()
+      }
+      let newText = text + targetText;
 
-  // target.addEventListener(type, listener[, options]);
-});
+
+      input.innerText = newText;
+      setCurrentValue(newText);
+    } else if (e.target.className === "operators") {
+      let operators = e.target.innerText;
+
+      if (isOp(operators)) {
+        setPrevValue();
+        clear();
+        setCurrentOperator(operators);
+        console.log(prevValue, currentOperator);
+      } else if (operators === "=") {
+        calculate();
+      }
+    } else if (e.target.className === "dot") {
+      let text = input.innerText;
+   
+      // console.log("sss",text.indexOf(".") );
+      if(text.indexOf(".") === -1 ){
+        // console.log(text.lenght);
+        // console.log(input.innerText.length);
+        if(input.innerText.length < 1){
+          input.innerText = "0";
+          // console.log("qqq")
+        }
+        // console.log(text.length);
+        // text = prevValue + ".";
+        input.innerText = input.innerText + "."
+      }
+  
+    }else if(e.target.className === "clear"){
+      clear();
+      // console.log("clear");
+    }else if(e.target.className === "delete"){
+      console.log("delete",input.innerText)
+      input.innerText = input.innerText.slice(0, input.innerText.length - 1);
+    }
+  });
+};
